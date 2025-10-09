9a641e204b6b28b87451fd42e8abb2c2d6d6d04a
Galagyy <109639693+Galagyy@users.noreply.github.com>
2025-10-08 22:36:25 -0700

    [Fix] Reset Project

--- Assets/Scripts/UI/Menu/Credits.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class Credits : MonoBehaviour {
-    public void Back() {
-        Debug.Log("Going back to menu.");
-        SceneManager.LoadScene("Menu");
-    }
-}
